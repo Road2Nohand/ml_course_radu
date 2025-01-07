a9d8265461d0914d288cf03f54b10fc30111e189
NiklasMickelat <niklas.mickelat@web.de>
2025-01-07 23:34:09 +0100

    progress indicator

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -20,7 +20,6 @@ function generateImageFile(outFile, paths) {
     draw.paths(CTX, paths);
     const buffer = CANVAS.toBuffer("image/png");
     fs.writeFileSync(outFile, buffer);
-    console.log("Saved .png of drawing");
 }
 
 //#endregion Functions
@@ -35,7 +34,6 @@ let id = 1;
 // go trough all files in /data/raw
 fileNames.forEach( fn => {
     full_fn = constants.RAW_DIR + "/" + fn
-    console.log("Reading File:", full_fn);
     const content = fs.readFileSync(full_fn);
     const {session, student, drawings} = JSON.parse(content);
     // get each drawing
@@ -51,7 +49,6 @@ fileNames.forEach( fn => {
         const paths = drawings[label];
         // save as JSON
         json_file_name_with_id = constants.JSON_DIR + "/" + id + ".json";
-        console.log(`Saved drawing ${label} from user ${student} with filename ${json_file_name_with_id}`);
         fs.writeFileSync(json_file_name_with_id, JSON.stringify(paths));
         
         // save as IMG
@@ -59,7 +56,6 @@ fileNames.forEach( fn => {
         generateImageFile(img_file_name_with_id, paths);
 
         utils.printProgress(id, fileNames.length*8); // 8 drawings per student
-
         id++;
     }
 });
